a62f88b0ed8ad27f6d22c1a42396651523a57a8c
Sekera Alexandr <sekera.alexandr.2017@skola.ssps.cz>
2020-11-18 19:15:40 +0100

    Vzhled upraven, místo pro logo, funkčnost nulová. Je problém s barvou pozadí, v aplikaci nevypadá tak, jak by měla.

--- LoginAndMainUI/Login.xaml.cs
+++ LoginAndMainUI/Login.xaml.cs
@@ -25,11 +25,40 @@ namespace LoginAndMainUI
             InitializeComponent();
             MainUI mainUI = new MainUI();
             mainUI.Show();
+            tbName.GotKeyboardFocus += new KeyboardFocusChangedEventHandler(tb_GotKeyboardFocus);
+            tbName.LostKeyboardFocus += new KeyboardFocusChangedEventHandler(tb_LostKeyboardFocus);
+        }
+        private void tb_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (sender is TextBox)
+            {
+                if (((TextBox)sender).Foreground == Brushes.Gray)
+                {
+                    ((TextBox)sender).Text = "";
+                    ((TextBox)sender).Foreground = Brushes.White;
+                }
+            }
+        }
+        private void tb_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (sender is TextBox)
+            {
+                if (((TextBox)sender).Text.Trim().Equals(""))
+                {
+                    ((TextBox)sender).Foreground = Brushes.Gray;
+                    ((TextBox)sender).Text = "Název účtu";
+                }
+            }
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             DragMove();
+        }
+
+        private void btnAccept_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
