6f495a9df3afe2ca56f58c89602f13a5aaf5e589
Kozler Jakub <kozler.jakub.2017@skola.ssps.cz>
2020-11-19 16:42:47 +0100

    Add every information about task. Add panel for checking tasks and more stuff.

--- LoginAndMainUI/MainUI.xaml.cs
+++ LoginAndMainUI/MainUI.xaml.cs
@@ -23,6 +23,8 @@ namespace LoginAndMainUI
     {
         int notificationCounter = 0;
         bool menuGloraIsEnabled = false;
+        bool infoGloraIsEnable = false;
+        bool taskBarIsShowen = true;
         public MainUI()
         {
             InitializeComponent();
@@ -65,6 +67,7 @@ namespace LoginAndMainUI
         {
             DoubleAnimation blurEnable = new DoubleAnimation(0, 0.9, new Duration(TimeSpan.FromSeconds(0.5)));
             informationProperities.BeginAnimation(OpacityProperty, blurEnable);
+            infoGloraIsEnable = true;
         }
 
         private void informationBarZero_MouseEnter(object sender, MouseEventArgs e)
@@ -83,6 +86,7 @@ namespace LoginAndMainUI
         {
             DoubleAnimation blurEnable = new DoubleAnimation(0.9, 0, new Duration(TimeSpan.FromSeconds(0.5)));
             informationProperities.BeginAnimation(OpacityProperty, blurEnable);
+            infoGloraIsEnable = false;
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
@@ -92,7 +96,65 @@ namespace LoginAndMainUI
             else if (e.Key == Key.I)
                 informationBar_Click(sender, e);
             else if (e.Key == Key.Escape)
-                closeNoti_Click(sender, e);
+            {
+                if (infoGloraIsEnable)
+                    closeNoti_Click(sender, e);
+                if (menuGloraIsEnabled)
+                    gloraHome_Click(sender, e);
+            }
+        }
+
+        private void moreInfoTask_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void moreInfoTask_MouseEnter(object sender, MouseEventArgs e)
+        {
+            moreInfoTask.Cursor = Cursors.Hand;
+            moreInfoTask.Foreground = new SolidColorBrush(Color.FromRgb(21,67,96));
+        }
+
+        private void moreInfoTask_MouseLeave(object sender, MouseEventArgs e)
+        {
+            moreInfoTask.Cursor = Cursors.Arrow;
+            moreInfoTask.Foreground = new SolidColorBrush(Color.FromRgb(46, 134, 193));
+        }
+
+        private void doneTaskLb_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void progressTaskLb_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void createTaskLb_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void failedTaskLb_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void minimzeTask_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation blurEnable = new DoubleAnimation(1, 0, new Duration(TimeSpan.FromSeconds(0.3)));
+            taskBarWholeInfo.BeginAnimation(OpacityProperty, blurEnable);
+            taskBarIsShowen = false;
+            showTaskBar.Visibility = Visibility.Visible;
+        }
+
+        private void showTaskBar_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation blurEnable = new DoubleAnimation(0, 1, new Duration(TimeSpan.FromSeconds(0.3)));
+            taskBarWholeInfo.BeginAnimation(OpacityProperty, blurEnable);
+            taskBarIsShowen = true;
+            showTaskBar.Visibility = Visibility.Hidden;
         }
     }
 }
