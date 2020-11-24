5ea16c63d66b0f2b14ff72a469242c3379ec1ae2
Sekera Alexandr <sekera.alexandr.2017@skola.ssps.cz>
2020-11-24 16:03:59 +0100

    Upraven vzhled loginu

--- LoginAndMainUI/Login.xaml.cs
+++ LoginAndMainUI/Login.xaml.cs
@@ -27,6 +27,7 @@ namespace LoginAndMainUI
             mainUI.Show();
             tbName.GotKeyboardFocus += new KeyboardFocusChangedEventHandler(tb_GotKeyboardFocus);
             tbName.LostKeyboardFocus += new KeyboardFocusChangedEventHandler(tb_LostKeyboardFocus);
+            tbPasswordReally.Visibility = Visibility.Collapsed;
         }
         private void tb_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
@@ -39,6 +40,7 @@ namespace LoginAndMainUI
                 }
             }
         }
+        string PHFraze = "Název účtu";
         private void tb_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
             if (sender is TextBox)
@@ -46,7 +48,7 @@ namespace LoginAndMainUI
                 if (((TextBox)sender).Text.Trim().Equals(""))
                 {
                     ((TextBox)sender).Foreground = Brushes.Gray;
-                    ((TextBox)sender).Text = "Název účtu";
+                    ((TextBox)sender).Text = PHFraze;
                 }
             }
         }
@@ -58,7 +60,59 @@ namespace LoginAndMainUI
 
         private void btnAccept_Click(object sender, RoutedEventArgs e)
         {
+            PHFraze = "Název účtu";
             Close();
+        }
+        bool IsRegistration = false;
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            tbName.Foreground = Brushes.Gray;
+            tbPassword.Focus();
+
+            if (IsRegistration == false)
+            {
+                tbPasswordReally.Visibility = Visibility.Visible;
+                PHFraze = "Registrační jméno";
+                tbName.Text = PHFraze;
+                tbPassword.Password = "";
+                tbPasswordReally.Password = tbPassword.Password;
+
+                Grid.SetRow(borderTBName, Grid.GetRow(borderTBName) - 1);
+                Grid.SetRowSpan(borderTBName, Grid.GetRowSpan(borderTBName) + 1);
+
+                Grid.SetRow(borderTBPassword, Grid.GetRow(borderTBPassword) - 1);
+                Grid.SetRowSpan(borderTBPassword, Grid.GetRowSpan(borderTBPassword) + 1);
+
+                Grid.SetRowSpan(borderTBPasswordReally, Grid.GetRowSpan(borderTBPasswordReally) + 1);
+
+                Grid.SetRowSpan(btnAccept, Grid.GetRowSpan(btnAccept) + 1);
+
+                btnAccept.Content = "Registrovat";
+                IsRegistration = true;
+                borderRegister.ToolTip = "Přihlásit se";
+            }
+            else 
+            {
+                PHFraze = "Název účtu";
+                tbPasswordReally.Visibility = Visibility.Collapsed;
+                tbName.Text = PHFraze;
+                tbPassword.Password = "";
+                tbPasswordReally.Password = tbPassword.Password;
+
+                Grid.SetRow(borderTBName, Grid.GetRow(borderTBName) + 1);
+                Grid.SetRowSpan(borderTBName, Grid.GetRowSpan(borderTBName) - 1);
+
+                Grid.SetRow(borderTBPassword, Grid.GetRow(borderTBPassword) + 1);
+                Grid.SetRowSpan(borderTBPassword, Grid.GetRowSpan(borderTBPassword) - 1);
+
+                Grid.SetRowSpan(borderTBPasswordReally, Grid.GetRowSpan(borderTBPasswordReally) - 1);
+
+                Grid.SetRowSpan(btnAccept, Grid.GetRowSpan(btnAccept) - 1);
+
+                btnAccept.Content = "Přihlásit se";
+                IsRegistration = false;
+                borderRegister.ToolTip = "Registrovat";
+            }
         }
     }
 }
